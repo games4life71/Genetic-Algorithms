7e55b9d6a6cd9ffad7df65e762315e7a5de58930
Stefan <7stefanadrian@gmail.com>
2022-11-08 21:09:55 +0200

    added short info

---
+++ Relief Fitness/main.py
@@ -0,0 +1,8 @@
+#begin with a randomly chosen generation 1st generation 
+#evaluate each chromosome to test how well it solves the problem 
+#select based on evaluate which chromosomes will reproduce 
+#cross over the chromosomes 
+#mutate with a very low probability
+# 
+#  
+
