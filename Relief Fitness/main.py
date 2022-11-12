f025fe729100410a696f45450474d9a25d63533d
Stefan <7stefanadrian@gmail.com>
2022-11-12 23:46:32 +0200

    implement fitness_select

--- Relief Fitness/main.py
+++ Relief Fitness/main.py
@@ -6,9 +6,17 @@
 
 import numpy as np
 from numba import jit
+import math
 
-# generating the starting population
+# generating the starting 
+DEJON_INTERV = (-5.12,5.12)
 POPULATION_SIZE = 10
+PRECISION = 5
+N_DEJON = math.trunc(math.log2((DEJON_INTERV[1]- DEJON_INTERV[0])*pow(10,PRECISION))) #number of bits required
+
+
+def De_Jong(params):
+   return sum(val**2 for val in params)
 
 @jit(parallel = True)
 def generate_chromosome(func_params, bit_count):
@@ -50,10 +58,71 @@ def mutate_gene(chromo, funct_params, bit_count):
         chromo[position] = 1
 
     return chromo
+    
+
+def binatodeci(bitstring)->int: 
+    return sum(val*(2**idx) for idx, val in enumerate(reversed(bitstring)))
+
+
+#Decode it so it fits into function interval 
+@jit(parallel = True)
+def decode(solution, a, b, no_of_bits)->float:
+    return (a + binatodeci(solution)*(b-a)/(pow(2, no_of_bits)-1))
+
+
+def evaluate_fitness(population,no_of_params,function_name):
+     fitness_population :list = []
+     for chrom  in population:  
+        #params decoded into real values 
+        params_decode = (decode(x,DEJON_INTERV[0],DEJON_INTERV[1],N_DEJON) for x in  np.split(chrom,no_of_params))        
+        #print(params_decode)
+        fitness_population.append(1/function_name(params_decode))
+        total = sum(x for x in fitness_population)
+        fitness_final = []
+        fitness_cumulative = []
+        
+        for i in range(len(fitness_population)):
+            fitness_final.append(fitness_population[i]/total)
+            if i == 0 : 
+                fitness_cumulative.append(fitness_final[i])
+            else :
+                fitness_cumulative.append(fitness_cumulative[i-1]+fitness_final[i]) 
+
+
+     return (fitness_final,fitness_cumulative)
+
+
+def print_fitness(fitness_pop):
+    for entry in fitness_pop:
+        print(entry)
+
+
+#select using roulette-wheel
+def select_chromosome(pop_cumul:list,population):
+    probab = np.random.random(POPULATION_SIZE)
+    new_gen = []
+    for i in range(len(probab)):
+        for j in range(len(pop_cumul)): 
+            if probab[i]<pop_cumul[j]:
+                new_gen.append(population[j])
+                break
+    return new_gen
+    
+
+    
+    
 if __name__ == "__main__":
     start_population = generate_starting_pop(2, 2)
-    print_population(start_population)
+    #print_population(start_population)
 
     # print(cross_over(start_population[0].tolist(),
     #       start_population[1].tolist(), 2, 2))
-    print(mutate_gene(start_population[0].tolist(),2,2))
+    #print(mutate_gene(start_population[0].tolist(),2,2))
+    fitness_info= De_Jong_Fitness(start_population,2)
+    new_gen =select_chromosome(fitness_info[1],start_population)
+    print_population(new_gen)
+    # print_fitness(fitness_info[1])
+    # print_fitness(fitness_info[0])
+    #print(f'total is {fitness_info[0]}')
+    
+    pass
