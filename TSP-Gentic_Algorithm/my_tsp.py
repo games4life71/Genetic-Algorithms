966a0645cd06f28f466dbdb5c63895ae7a8310eb
Jitca Diana <jitcadiana6@gmail.com>
2022-12-19 17:49:39 +0200

    todo list

--- TSP-Gentic_Algorithm/my_tsp.py
+++ TSP-Gentic_Algorithm/my_tsp.py
@@ -1,46 +1,56 @@
+#genetic algorithm that solves the Travel Salesman 
+#we have : parser, dict with the number of city and the coordonates, distance between 2 cities
+# TODO : first generation, fitness 
 import numpy as np
-import pandas as pd
+import random
 
-locations  =[]
-with open("bays29.txt", 'r') as fp:
-    # read all lines in a list
-    lines = fp.readlines()
-    for line, content in enumerate(lines):
-        # check if string present on a current line
-        if line>7:
-            x = content.split()
-            final = " ".join(x[1:])
-            location1 = final.split(' ')
-            location = (float(location1[0]), float(location1[1]))
-            locations.append(location)
+def parse_file(file_name):
+    locations  =[]
+    cities_id  =[]
+    with open("bays29.txt", 'r') as fp:
+        # read all lines in a list
+        lines = fp.readlines()
+        for line, content in enumerate(lines):
+            # check if string present on a current line
+            if line>7:
+                x = content.split() # split after each space
 
-size = len(locations)
+                
+                cities_id.append(x[0])
+                coords = " ".join(x[1:])
+                
 
-# distances = [] #deasupra diagonalei principale
-# for i in range(0, size):
-#     for j in range(i+1, size):
-#             distance = np.sqrt((abs(locations[j][0]-locations[i][0]) ** 2) + (abs(locations[j][1]-locations[i][1]) ** 2))
-#             distances.append(distance)
-# print(distances)
+                location1 = coords.split(' ')
+                location = (float(location1[0]), float(location1[1]))
+                locations.append(location)
 
-# class City:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-    
-#     def distance(self, city):
-#         xDis = abs(self.x - city.x)
-#         yDis = abs(self.y - city.y)
-#         distance = np.sqrt((xDis ** 2) + (yDis ** 2))
-#         return distance
-    
-#     def __repr__(self):
-#         return "(" + str(self.x) + "," + str(self.y) + ")"
+    #make a dict containing the cities and their coordinates
+    cities = { x:y for x,y, in zip(cities_id, locations)}
+    #print(cities)
+    return cities
 
-# cityList = []
 
-# for city in locations:
-#     cityList.append(City(city[0], city[1]))
+def print_dict (dict):
+    for key, value in dict.items():
+        print(key, value)
 
-# for i in range(0, size):
-#     print(cityList[0].distance(cityList[i]))
+
+#distance betweeen two cities
+def distance_two_points (city1, city2):
+    xDis = abs(city1[0] - city2[0])
+    yDis = abs(city1[1] - city2[1])
+    distance = np.sqrt((xDis ** 2) + (yDis ** 2))
+    return distance
+
+
+distances = []
+
+cities_dict = parse_file("bays29.txt") #parse the file and return a dict with the cities and their coordinates
+print_dict(cities_dict)
+
+
+def createRoute(cityList):
+    route = random.sample(cityList, len(cityList))
+    return route
+
+print(createRoute(cities_dict))
